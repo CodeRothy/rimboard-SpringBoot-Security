bd8b5902b7abf0ef7d71b17526e9900a69b1b622
CodeRothy <yiwooy@naver.com>
2022-04-26 01:54:08 +0900

    Initial commit

---
+++ src/main/java/kr/rimu/rimboard/RimboardApplication.java
@@ -0,0 +1,13 @@
+package kr.rimu.rimboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RimboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/rimu/rimboard/RimboardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.rimu.rimboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RimboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
